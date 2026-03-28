913f6a795995a05330370036bad242e97741a209
VxidDev <stas050595@gmail.com>
2026-03-28 19:57:48 +0100

    Updated tiny.py to follow best usage practices.

--- examples/tiny.py
+++ examples/tiny.py
@@ -1,4 +1,4 @@
-from nebula import Nebula , run_dev
+from nebula import Nebula
 from nebula.response import HTMLResponse
 
 app = Nebula(__file__, "localhost", 8000)
@@ -7,4 +7,5 @@ app = Nebula(__file__, "localhost", 8000)
 async def root(request) -> None:
     return "<h1>Welcome to Nebula!</h1>"
 
-run_dev(app)
+if __name__ == "__main__":
+    app.run()
